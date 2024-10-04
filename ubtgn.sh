d91171e522cdbcdc4256fe0fa71238d0f875f755
1Computer.PK <gb.lazadadc001@gmail.com>
2024-10-05 01:21:26 +0700

    Update ubtgn.sh

--- ubtgn.sh
+++ ubtgn.sh
@@ -10,7 +10,7 @@ sudo nala install anacron apt-xapian-index at-spi2-core colord cups dbus-x11 dmz
 
 sudo nala update
 
-sudo nala install 7zip exfatprogs ntfs-3g libqt5widgets5 lz4 mtools mmv binfmt-support libusb-1.0-0-dev zstd blueman bluez bluez-cups bluez-tools cmst fonts-liberation fonts-nanum fonts-sawarabi-gothic fonts-tlwg-waree pcmanfm ghex galculator gparted gnome-packagekit gnome-packagekit-common gnome-package-updater mousepad nemo nemo-data nemo-fileroller nemo-font-manager numix-icon-theme-circle viewnior btop gnome-tweaks -y
+sudo nala install 7zip exfatprogs ntfs-3g libqt5widgets5 lz4 mtools mmv binfmt-support libusb-1.0-0-dev zstd blueman bluez bluez-cups bluez-tools cmst fonts-liberation fonts-nanum fonts-sawarabi-gothic fonts-tlwg-waree pcmanfm ghex galculator gparted gnome-packagekit gnome-packagekit-common gnome-package-updater mousepad nemo nemo-data nemo-fileroller nemo-font-manager papirus-icon-theme viewnior btop gnome-tweaks -y
 
 sudo nala update
 
