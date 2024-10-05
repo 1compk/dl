70c582583da0dbee04e785307c91c019973855d7
1Computer.PK <gb.lazadadc001@gmail.com>
2024-10-06 03:19:21 +0700

    Add files via upload

---
+++ Packages/slax.sh
@@ -0,0 +1,9 @@
+apt update -y && apt install nala -y && nala fetch
+
+nala update && nala upgrade -y
+
+nala install 7zip exfatprogs ntfs-3g libqt5widgets5 lz4 mtools mmv zstd fonts-liberation fonts-nanum fonts-sawarabi-gothic fonts-tlwg-waree pcmanfm ghex gparted mousepad nemo nemo-data nemo-fileroller viewnior eject gnome-disk-utility
+
+nala update
+
+nala clean
