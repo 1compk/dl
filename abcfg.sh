16c5f7d4f775d26fefabb1095e17ac5b6a921fce
1Computer.PK <gb.lazadadc001@gmail.com>
2024-10-06 03:19:50 +0700

    Add files via upload

---
+++ abcfg.sh
@@ -0,0 +1,5 @@
+sudo apt update -y && sudo apt install nala -y && sudo nala fetch
+
+sudo nala update && sudo nala upgrade -y
+
+sudo nala install armbian-config -y && sudo armbian-config
---
+++ ubtdt.sh
@@ -0,0 +1,25 @@
+sudo apt update -y && sudo apt install nala -y && sudo nala fetch
+
+sudo nala update && sudo nala upgrade -y
+
+sudo nala install papirus-icon-theme adwaita-icon-theme -y
+
+sudo nala update
+
+sudo nala install gdm3 -y
+
+sudo nala update
+
+sudo nala install anacron apt-xapian-index at-spi2-core colord cups dbus-x11 dmz-cursor-theme dconf-cli eject foomatic-db-compressed-ppds gdebi gnome-control-center gnome-disk-utility gnome-desktop3-data gnome-menus gnome-screenshot gnome-system-monitor gnome-terminal gnome-packagekit gnome-session gnome-shell gnome-shell-extension-appindicator inputattach libnotify-bin lm-sensors pavucontrol profile-sync-daemon pulseaudio pulseaudio-module-bluetooth software-properties-gtk synaptic system-config-printer terminator tracker tracker-extract tracker-miner-fs upower x11-apps x11-session-utils x11-utils x11-xserver-utils xarchiver xdg-user-dirs xdg-user-dirs-gtk xfonts-base xserver-xorg xwayland zenity -y
+
+sudo nala update
+
+sudo nala install 7zip exfatprogs ntfs-3g libqt5widgets5 lz4 mtools mmv binfmt-support libusb-1.0-0-dev zstd blueman bluez bluez-cups bluez-tools cmst fonts-liberation fonts-nanum fonts-sawarabi-gothic fonts-tlwg-waree pcmanfm ghex galculator gparted gnome-packagekit gnome-packagekit-common gnome-package-updater mousepad nemo nemo-data nemo-fileroller nemo-font-manager viewnior btop gnome-tweaks iwgtk network-manager-gnome -y
+
+sudo nala update
+
+sudo nala install anacron apt-xapian-index blueman bluez bluez-cups bluez-tools brltty brltty-x11 cifs-utils cinnamon cinnamon-common cinnamon-control-center cinnamon-control-center-data cinnamon-core cinnamon-desktop-data cinnamon-screensaver cinnamon-session cinnamon-session-common cinnamon-settings-daemon colord cups cups-bsd cups-client cups-filters dbus-x11 dconf-cli evince evince-common fontconfig fontconfig-config foomatic-db-compressed-ppds gdebi ghostscript-x gir1.2-cinnamondesktop-3.0 gir1.2-cmenu-3.0 gir1.2-cvc-1.0 gnome-disk-utility gnome-screenshot gnome-system-monitor gnome-terminal gtk2-engines gtk2-engines-murrine gtk2-engines-pixbuf gvfs-backends indicator-printers inxi inputattach keyutils laptop-detect libatk-adaptor libcinnamon-desktop4 libcinnamon-menu-3-0 libcscreensaver0 libcvc0 libgl1-mesa-dri libgsettings-qt1 libgtk2.0-bin libnotify-bin libxcursor1 lm-sensors mesa-utils mousepad mousetweaks nemo nemo-data nemo-fileroller nemo-font-manager nemo-gtkhash openprinting-ppds p7zip-full pamix pavucontrol policykit-1 policykit-desktop-privileges profile-sync-daemon pulseaudio-module-bluetooth redshift slick-greeter smbclient software-properties-gtk spice-vdagent system-config-printer system-config-printer-common update-manager update-manager-core viewnior x11-apps x11-utils x11-xserver-utils xapps-common xarchiver xbacklight xcursor-themes xdg-user-dirs xdg-user-dirs-gtk xfonts-100dpi xfonts-75dpi xfonts-base xfonts-encodings xfonts-scalable xfonts-utils xorg-docs-core xsensors xserver-xorg xserver-xorg-video-fbdev xauth -y
+
+sudo nala update
+
+sudo nala clean
