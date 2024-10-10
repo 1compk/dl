958139661320e36e08880ec81999bd5461647bd6
1Computer.PK <gb.lazadadc001@gmail.com>
2024-10-10 20:36:36 +0700

    Add files via upload

---
+++ ubtcin.sh
@@ -0,0 +1,19 @@
+sudo apt update -y
+
+sudo apt install nala -y && sudo nala fetch
+
+sudo nala update && sudo nala upgrade -y
+
+sudo nala install papirus-icon-theme sddm lxterminal -y
+
+sudo nala update
+
+sudo nala install 7zip exfatprogs ntfs-3g libqt5widgets5 lz4 mtools mmv zstd blueman bluez bluez-cups bluez-tools fonts-liberation fonts-nanum fonts-sawarabi-gothic fonts-tlwg-waree pcmanfm ghex galculator gparted mousepad nemo nemo-data nemo-fileroller nemo-font-manager viewnior btop network-manager-gnome -y
+
+sudo nala update
+
+sudo nala install anacron apt-xapian-index blueman bluez bluez-cups bluez-tools cifs-utils cinnamon cinnamon-common cinnamon-control-center cinnamon-control-center-data cinnamon-core cinnamon-desktop-data cinnamon-session cinnamon-session-common cinnamon-settings-daemon colord cups cups-bsd cups-client cups-filters dbus-x11 dconf-cli evince evince-common fontconfig fontconfig-config foomatic-db-compressed-ppds gdebi ghostscript-x gir1.2-cinnamondesktop-3.0 gir1.2-cmenu-3.0 gir1.2-cvc-1.0 gnome-disk-utility gnome-screenshot gnome-system-monitor gtk2-engines gtk2-engines-murrine gtk2-engines-pixbuf gvfs-backends indicator-printers inxi inputattach keyutils laptop-detect libatk-adaptor libcinnamon-desktop4 libcinnamon-menu-3-0 libcscreensaver0 libcvc0 libgl1-mesa-dri libgsettings-qt1 libgtk2.0-bin libnotify-bin libxcursor1 lm-sensors mesa-utils mousepad mousetweaks nemo nemo-data nemo-fileroller nemo-font-manager nemo-gtkhash openprinting-ppds p7zip-full pamix pavucontrol policykit-1 policykit-desktop-privileges profile-sync-daemon pulseaudio-module-bluetooth redshift slick-greeter smbclient software-properties-gtk spice-vdagent system-config-printer system-config-printer-common update-manager update-manager-core viewnior x11-apps x11-utils x11-xserver-utils xapps-common xarchiver xbacklight xcursor-themes xdg-user-dirs xdg-user-dirs-gtk xfonts-100dpi xfonts-75dpi xfonts-base xfonts-encodings xfonts-scalable xfonts-utils xorg-docs-core xsensors xserver-xorg xserver-xorg-video-fbdev xauth -y
+
+sudo nala update
+
+sudo nala clean
