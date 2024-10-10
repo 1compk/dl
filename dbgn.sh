33f2e7cbd59316a8ccb69cbefb32bc39be573565
1Computer.PK <gb.lazadadc001@gmail.com>
2024-10-11 04:17:01 +0700

    Update dbgn.sh

--- dbgn.sh
+++ dbgn.sh
@@ -4,11 +4,11 @@ sudo apt install nala -y && sudo nala fetch
 
 sudo nala update && sudo nala upgrade -y
 
-sudo nala install papirus-icon-theme adwaita-icon-theme gdm3 -y
+sudo nala install papirus-icon-theme gdm3 gnome-terminal -y
 
 sudo nala update
 
-sudo nala install 7zip exfatprogs ntfs-3g libqt5widgets5 lz4 mtools mmv zstd blueman bluez bluez-cups bluez-tools fonts-liberation fonts-nanum fonts-sawarabi-gothic fonts-tlwg-waree pcmanfm ghex galculator gparted mousepad nemo nemo-data nemo-fileroller nemo-font-manager viewnior btop network-manager-gnome gnome-tweaks grub-customizer gnome-packagekit gnome-packagekit-common gnome-package-updater -y
+sudo nala install 7zip exfatprogs ntfs-3g libqt5widgets5 lz4 mtools mmv zstd blueman bluez bluez-cups bluez-tools fonts-liberation fonts-nanum fonts-sawarabi-gothic fonts-tlwg-waree pcmanfm ghex gparted mousepad nemo nemo-data nemo-fileroller nemo-font-manager btop network-manager-gnome gnome-tweaks grub-customizer gnome-packagekit gnome-packagekit-common gnome-package-updater -y
 
 sudo nala update
 
