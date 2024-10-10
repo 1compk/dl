e62482b5b9915f7399399f3935302e476b94e5de
1Computer.PK <gb.lazadadc001@gmail.com>
2024-10-11 04:18:48 +0700

    Update ubtgn.sh

--- ubtgn.sh
+++ ubtgn.sh
@@ -4,11 +4,11 @@ sudo apt install nala -y && sudo nala fetch
 
 sudo nala update && sudo nala upgrade -y
 
-sudo apt install papirus-icon-theme adwaita-icon-theme gdm3 -y
+sudo apt install papirus-icon-theme gdm3 gnome-terminal -y
 
 sudo nala update
 
-sudo apt install 7zip exfatprogs ntfs-3g libqt5widgets5 lz4 mtools mmv zstd blueman bluez bluez-cups bluez-tools fonts-liberation fonts-nanum fonts-sawarabi-gothic fonts-tlwg-waree pcmanfm ghex galculator gparted mousepad nemo nemo-data nemo-fileroller nemo-font-manager viewnior btop network-manager-gnome gnome-tweaks -y
+sudo apt install 7zip exfatprogs ntfs-3g libqt5widgets5 lz4 mtools mmv zstd blueman bluez bluez-cups bluez-tools fonts-liberation fonts-nanum fonts-sawarabi-gothic fonts-tlwg-waree pcmanfm ghex gparted mousepad nemo nemo-data nemo-fileroller nemo-font-manager btop network-manager-gnome gnome-tweaks -y
 
 sudo nala update
 
