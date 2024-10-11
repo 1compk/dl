6978797b0d09d008d5110dbcc801eaf148e4b480
1Computer.PK <gb.lazadadc001@gmail.com>
2024-10-11 23:19:25 +0700

    Add files via upload

---
+++ ubtcg.sh
@@ -0,0 +1,23 @@
+sudo apt update -y
+
+sudo apt install nala -y
+
+sudo nala update && sudo nala upgrade -y
+
+sudo apt install papirus-icon-theme gdm3 gnome-terminal gedit -y
+
+sudo nala update
+
+sudo apt install 7zip exfatprogs ntfs-3g libqt5widgets5 lz4 mtools mmv zstd blueman bluez bluez-cups bluez-tools fonts-liberation fonts-nanum fonts-sawarabi-gothic fonts-tlwg-waree pcmanfm ghex gparted nemo nemo-data nemo-fileroller nemo-font-manager btop gnome-tweaks -y
+
+sudo nala update
+
+sudo apt install anacron apt-xapian-index at-spi2-core colord cups dbus-x11 dconf-cli eject foomatic-db-compressed-ppds gdebi gnome-control-center gnome-disk-utility gnome-desktop3-data gnome-menus gnome-screenshot gnome-system-monitor gnome-packagekit gnome-session gnome-shell gnome-shell-extension-appindicator inputattach libnotify-bin lm-sensors pavucontrol profile-sync-daemon pulseaudio pulseaudio-module-bluetooth software-properties-gtk system-config-printer tracker tracker-extract tracker-miner-fs upower x11-apps x11-session-utils x11-utils x11-xserver-utils xarchiver xdg-user-dirs xdg-user-dirs-gtk xfonts-base xserver-xorg xwayland zenity blueman bluez bluez-cups bluez-tools cifs-utils cinnamon cinnamon-common cinnamon-control-center cinnamon-control-center-data cinnamon-core cinnamon-desktop-data cinnamon-session cinnamon-session-common cinnamon-settings-daemon cups-bsd cups-client cups-filters evince evince-common fontconfig fontconfig-config ghostscript-x gir1.2-cinnamondesktop-3.0 gir1.2-cmenu-3.0 gir1.2-cvc-1.0 gtk2-engines gtk2-engines-murrine gtk2-engines-pixbuf gvfs-backends indicator-printers inxi keyutils laptop-detect libatk-adaptor libcinnamon-desktop4 libcinnamon-menu-3-0 libcscreensaver0 libcvc0 libgl1-mesa-dri libgsettings-qt1 libgtk2.0-bin libxcursor1 mesa-utils mousetweaks nemo nemo-data nemo-fileroller nemo-font-manager nemo-gtkhash openprinting-ppds p7zip-full pamix policykit-1 policykit-desktop-privileges redshift slick-greeter smbclient spice-vdagent system-config-printer-common update-manager update-manager-core xapps-common xbacklight xcursor-themes xfonts-100dpi xfonts-75dpi xfonts-base xfonts-encodings xfonts-scalable xfonts-utils xorg-docs-core xsensors xserver-xorg-video-fbdev xauth -y
+
+sudo nala update
+
+sudo apt install sddm -y
+
+sudo nala update
+
+sudo nala clean
