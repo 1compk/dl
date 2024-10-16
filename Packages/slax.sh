c43897c13d807208ea5c396d3a5293b7bb736f56
1Computer.PK <gb.lazadadc001@gmail.com>
2024-10-16 22:23:09 +0700

    Add files via upload

---
+++ Packages/slax.sh
@@ -0,0 +1,23 @@
+apt update -y
+
+apt install nala -y && nala fetch
+
+nala update && nala upgrade -y
+
+nala install papirus-icon-theme gdm3 gnome-terminal gedit 7zip exfatprogs ntfs-3g libqt5widgets5 lz4 mtools mmv zstd blueman bluez bluez-cups bluez-tools fonts-liberation fonts-nanum fonts-sawarabi-gothic fonts-tlwg-waree pcmanfm ghex gparted nemo nemo-data nemo-fileroller nemo-font-manager btop gnome-tweaks grub-customizer gnome-packagekit gnome-packagekit-common -y
+
+nala update
+
+nala install anacron apt-xapian-index at-spi2-core colord cups dbus-x11 dconf-cli eject foomatic-db-compressed-ppds gdebi gnome-calculator gnome-control-center gnome-disk-utility gnome-desktop3-data gnome-menus gnome-screenshot gnome-system-monitor gnome-terminal gnome-session gnome-shell inputattach libnotify-bin libpulsedsp gdm3 lm-sensors pavucontrol profile-sync-daemon pulseaudio pulseaudio-module-bluetooth software-properties-gtk system-config-printer tracker tracker-extract tracker-miner-fs upower x11-apps x11-session-utils x11-utils x11-xserver-utils xarchiver xdg-user-dirs xdg-user-dirs-gtk xfonts-base xserver-xorg xwayland zenity gedit viewnior -y
+
+nala update
+
+nala install anacron apt-xapian-index blueman bluez bluez-cups bluez-tools cifs-utils cinnamon cinnamon-common cinnamon-control-center cinnamon-control-center-data cinnamon-core cinnamon-desktop-data cinnamon-session cinnamon-session-common cinnamon-settings-daemon colord cups cups-bsd cups-client cups-filters dbus-x11 dconf-cli evince evince-common fontconfig fontconfig-config foomatic-db-compressed-ppds gdebi ghostscript-x gir1.2-cinnamondesktop-3.0 gir1.2-cmenu-3.0 gir1.2-cvc-1.0 gnome-disk-utility gnome-screenshot gnome-system-monitor gnome-terminal gtk2-engines gtk2-engines-murrine gtk2-engines-pixbuf gvfs-backends inxi inputattach keyutils laptop-detect libatk-adaptor libcinnamon-desktop4 libcinnamon-menu-3-0 libcvc0 libgl1-mesa-dri libgsettings-qt1 libgtk2.0-bin libnotify-bin libxcursor1 lm-sensors mesa-utils mousetweaks nemo nemo-data nemo-fileroller nemo-font-manager nemo-gtkhash openprinting-ppds p7zip-full pamix pavucontrol polkitd pkexec profile-sync-daemon pulseaudio pulseaudio-module-bluetooth redshift slick-greeter smbclient software-properties-gtk spice-vdagent system-config-printer system-config-printer-common viewnior x11-apps x11-utils x11-xserver-utils xapps-common xarchiver xbacklight xcursor-themes xdg-user-dirs xdg-user-dirs-gtk xfonts-100dpi xfonts-75dpi xfonts-base xfonts-encodings xfonts-scalable xfonts-utils xorg-docs-core xsensors xserver-xorg xserver-xorg-video-fbdev xauth galculator gedit -y
+
+nala update
+
+nala install sddm -y
+
+nala update
+
+nala clean
