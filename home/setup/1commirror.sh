25bfe18b5beb6aad405a5a695d0b1adeb0a25d3a
1Computer.PK <gb.lazadadc001@gmail.com>
2025-09-03 04:40:44 +0700

    Delete home/setup directory

--- home/setup/1commirror.sh
+++
@@ -1,2 +0,0 @@
-echo "deb https://mirror.sg.gs/debian/ bookworm main" | sudo tee -a /etc/apt/sources.list
-echo "deb https://mirror.twds.com.tw/debian/ bookworm main" | sudo tee -a /etc/apt/sources.list
