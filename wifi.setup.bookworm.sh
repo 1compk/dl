50ae7b6359392a4a5828fd8f6f80f992833c9e06
1Computer.PK <gb.lazadadc001@gmail.com>
2025-09-24 21:32:19 +0700

    Update wifi.setup.bookworm.sh

--- wifi.setup.bookworm.sh
+++ wifi.setup.bookworm.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#echo "deb http://debian-archive.trafficmanager.net/debian/ bookworm main" > /etc/apt/sources.list.d/nala-bookworm.list
+#echo "deb https://mirror.sg.gs/debian/ bookworm main" >> /etc/apt/sources.list.d/nala-bookworm.list
+#echo "deb https://mirror.twds.com.tw/debian/ bookworm main" >> /etc/apt/sources.list.d/nala-bookworm.list
 rsync -avP nala-bookworm.list /etc/apt/sources.list.d/
 
 # Unblock wifi
