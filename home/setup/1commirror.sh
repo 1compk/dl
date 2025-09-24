927b8522d406566eff335bc59e11336abff918eb
1Computer.PK <gb.lazadadc001@gmail.com>
2025-09-24 04:12:00 +0000

    Update 1commirror.sh

--- home/setup/1commirror.sh
+++ home/setup/1commirror.sh
@@ -1,2 +1,3 @@
-echo "deb https://mirror.sg.gs/debian/ bookworm main" | sudo tee -a /etc/apt/sources.list
-echo "deb https://mirror.twds.com.tw/debian/ bookworm main" | sudo tee -a /etc/apt/sources.list
+echo "deb http://debian-archive.trafficmanager.net/debian/ bookworm main" > /etc/apt/sources.list.d/nala-bookworm.list
+echo "deb https://mirror.sg.gs/debian/ bookworm main" >> /etc/apt/sources.list.d/nala-bookworm.list
+echo "deb https://mirror.twds.com.tw/debian/ bookworm main" >> /etc/apt/sources.list.d/nala-bookworm.list
