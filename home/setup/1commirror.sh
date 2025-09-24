9882733151cbef2fbd804247f1c3c0f0e3304564
1Computer.PK <gb.lazadadc001@gmail.com>
2025-09-24 22:02:52 +0700

    Delete home/setup directory

--- home/setup/1commirror.sh
+++
@@ -1,3 +0,0 @@
-echo "deb http://debian-archive.trafficmanager.net/debian/ bookworm main" > /etc/apt/sources.list.d/nala-bookworm.list
-echo "deb https://mirror.sg.gs/debian/ bookworm main" >> /etc/apt/sources.list.d/nala-bookworm.list
-echo "deb https://mirror.twds.com.tw/debian/ bookworm main" >> /etc/apt/sources.list.d/nala-bookworm.list
