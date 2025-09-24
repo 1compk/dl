40c21bcfbd6cfd82306bc75a5cd9542828498148
1Computer.PK <gb.lazadadc001@gmail.com>
2025-09-24 04:07:04 +0000

    Add files via upload

---
+++ wifi.setup.bookworm.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+rsync -avP nala-bookworm.list /etc/apt/sources.list.d/
+
+# Unblock wifi
+rfkill unblock all
+rfkill unblock wifi
+
+#Scan for wifi :
+nmcli d wifi list
+
+# Prompt for New User and Password
+read -p "Enter SSID : " nmssid
+read -p "Enter Password : " wifipass
+nmcli d wifi connect "$nmssid" password "$wifipass"
+
+ping -c 2 1.1.1.1
+
+apt update -y && apt install nala systemd-timesyncd perl sudo -y
+
+nala --install-completion bash
+
+nala clean
+
+deluser --remove-home guest
+ls /home
+
+nala update && apt upgrade -y
+
+nala clean
+
+nala install bash-completion elpa-bash-completion network-manager -y
+
+nala clean
