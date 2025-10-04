15e93f0e51951146e23c404a758fd447955808e8
1Computer.PK <gb.lazadadc001@gmail.com>
2025-10-04 10:13:35 +0700

    Add files via upload

---
+++ home/setup/1commirror.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "deb https://mirror.sg.gs/debian/ bookworm main" > /etc/apt/sources.list.d/nala-bookworm.list
+echo "deb https://mirror.twds.com.tw/debian/ bookworm main" >> /etc/apt/sources.list.d/nala-bookworm.list
