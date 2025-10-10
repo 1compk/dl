71b7bf5642faf00f981783bf25f4aab0dcb7e4aa
1Computer.PK <gb.lazadadc001@gmail.com>
2025-10-11 02:40:22 +0700

    Add files via upload

---
+++ home/setup/boxde.sh
@@ -0,0 +1,36 @@
+#sudo dpkg --add-architecture i386
+#!/bin/bash
+
+echo "Updating..."
+sudo apt update -y
+
+echo "Installing Nala..."
+sudo apt install nala -y
+
+echo "Installing Nala Completion Bash..."
+sudo nala --install-completion bash
+
+echo "Updating..."
+sudo apt update -y && sudo apt upgrade -y
+
+echo "Cleaning..."
+sudo nala clean
+
+echo "Installing BoxDE..."
+xargs -a boxde sudo apt install
+
+echo "Updating..."
+sudo apt update -y
+
+echo "Cleaning..."
+sudo nala clean
+
+echo "Setting Desktop Manager..."
+sudo dpkg-reconfigure sddm
+
+echo "Setting Timezone..."
+sudo dpkg-reconfigure tzdata
+
+echo "Setting Completed..."
+echo "Now Plz Reboot by : sudo reboot"
+
