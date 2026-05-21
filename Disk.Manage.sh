432850019f3b9cba8e21861593e336a52a527957
1Computer.PK <gb.lazadadc001@gmail.com>
2026-05-21 22:31:31 +0700

    Add files via upload

--- Disk.Manage.sh
+++ Disk.Manage.sh
@@ -168,7 +168,7 @@ create_gpt_partitions() {
 
   sudo umount "$p3" 2>/dev/null || true
   #sudo mkfs.ext4 -F -O "^has_journal,sparse_super" -m 0 "$p3" || die "mkfs.ext4 failed on $p3"
-  sudo mkfs.xfs -f -i sparse=0 -b size=4096 "$p3" || die "mkfs.xfs failed on $p3"
+  sudo mkfs.xfs -f -i sparse=0 "$p3" || die "mkfs.xfs failed on $p3"
   sudo sync
   sudo partprobe "$disk" || true
   sudo udevadm settle || true
@@ -225,7 +225,7 @@ create_mbr_partitions() {
   echo "Formatting $p2 as ext4"
   sudo umount "$p2" 2>/dev/null || true
   #sudo mkfs.ext4 -F -O "^has_journal,sparse_super" -m 0 "$p2" || die "mkfs.ext4 failed on $p2"
-  sudo mkfs.xfs -f -i sparse=0 -b size=4096 "$p2" || die "mkfs.xfs failed on $p2"
+  sudo mkfs.xfs -f -i sparse=0 "$p2" || die "mkfs.xfs failed on $p2"
 
   sudo sync
   sudo partprobe "$disk" || true
