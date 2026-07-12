833fbef62032f112f96bba72f0a1581a760496e4
1Computer.PK <gb.lazadadc001@gmail.com>
2026-07-13 04:11:43 +0700

    Add files via upload

--- Disk.Manage.sh
+++ Disk.Manage.sh
@@ -167,10 +167,10 @@ create_gpt_partitions() {
   sudo udevadm settle || true
 
   sudo umount "$p3" 2>/dev/null || true
-  #sudo mkfs.ext4 -F -O "^has_journal,sparse_super" -m 0 "$p3" || die "mkfs.ext4 failed on $p3"
-  sudo mkfs.xfs -f -i sparse=0 -s size=4096 -b size=4096 "$p3" || die "mkfs.xfs failed on $p3"
+  sudo mkfs.ext4 -F -O "^has_journal,sparse_super" -m 0 "$p3" || die "mkfs.ext4 failed on $p3"
+  #sudo mkfs.xfs -f -i sparse=0 -s size=4096 -b size=4096 "$p3" || die "mkfs.xfs failed on $p3"
   #sudo mkfs.btrfs -fv -n 8K "$p3" || die "mkfs.btrfs failed on $p3"
-  #sudo mkfs.f2fs -f -o 0 "$p3" || die "mkfs.f2fs failed on $p3"
+  #sudo mkfs.f2fs -f -a 1 -o 10 "$p3" || die "mkfs.f2fs failed on $p3"
 
   sudo sync
   sudo partprobe "$disk" || true
@@ -227,10 +227,10 @@ create_mbr_partitions() {
   # 6. Format Partition 2 as EXT4
   echo "Formatting $p2 as ext4"
   sudo umount "$p2" 2>/dev/null || true
-  #sudo mkfs.ext4 -F -O "^has_journal,sparse_super" -m 0 "$p2" || die "mkfs.ext4 failed on $p2"
-  sudo mkfs.xfs -f -i sparse=0 -s size=4096 -b size=4096 "$p2" || die "mkfs.xfs failed on $p2"
+  sudo mkfs.ext4 -F -O "^has_journal,sparse_super" -m 0 "$p2" || die "mkfs.ext4 failed on $p2"
+  #sudo mkfs.xfs -f -i sparse=0 -s size=4096 -b size=4096 "$p2" || die "mkfs.xfs failed on $p2"
   #sudo mkfs.btrfs -fv -n 8K "$p2" || die "mkfs.btrfs failed on $p2"
-  #sudo mkfs.f2fs -f -o 0 "$p2" || die "mkfs.f2fs failed on $p2"
+  #sudo mkfs.f2fs -f -a 1 -o 10 "$p2" || die "mkfs.f2fs failed on $p2"
 
   sudo sync
   sudo partprobe "$disk" || true
